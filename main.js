b69c5fb450dc9f17a6c0873203cdaa3b9cc6cd3d
Marcos Vinicius Leal Faria <103334707+marcosvlf@users.noreply.github.com>
2022-07-06 08:44:09 -0300

    second commit

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-let boxes = document.getElementsByClassName('box');
-let winnerAnounce = document.getElementById('winner');
-let currentPlayer = 'X';
-let playerTurn = document.getElementById('turn');
-let playTurnXorO = playerTurn.innerHTML  = 'X';
-let resetBtn = document.getElementById('reset');
-let board = ['','','','','','','','',''];
-let gameOn = true ;
+let boxes = document.getElementsByClassName('box'); // variável que seleciona todas as divs com a classe 'box' transformando a em um HTML collection 
+let winnerAnnouce = document.getElementById('winner'); // variável que seleciona o id 'winner' para acessa-lo com o DOM.
+let currentPlayer = 'X'; // variável que atribui uma string com o valor 'x' para o current player
+let playerTurn = document.getElementById('turn'); //  variável que seleciona o id 'turn' para acess
+let resetBtn = document.getElementById('reset'); // variável que armazena o DOM do botão reset
+let board = ['','','','','','','','','']; // variavel que representa uma
+let gameOn = true;
 
-let winningConditions = [
+
+let winningConditions = [  // variavel onde é atribuida um array com 7 arrays dentro ,onde cada array representa a posição para que possa haver um vencedor.
+
     [0 , 1 , 2],
     [3 , 4 , 5],
     [6 , 7 , 8],
@@ -19,39 +20,31 @@ let winningConditions = [
 
 ]
 
-
-
-for (let i = 0; i < boxes.length; i++) {
-    let element = boxes[i];
-
+for (let i = 0; i < boxes.length; i++) {  // loop for que faz uma iteração para cada vez que houver um clique nos boxes.
+    let element = boxes[i];  // variável element onde é atribuida o valor da variavel boxes que é um HTML collection.
+  
     function changePlayer () {
-       
-        element.innerHTML = currentPlayer;
-        board[i] = currentPlayer;
+        element.innerHTML = currentPlayer;  // adiciona no HTML o valor do currentPlayer 
+        board[i] = currentPlayer;  // atribui para cada posição do array 'board' o valor do currentPlayer
 
-
-
-        if (currentPlayer === 'X'){
-            currentPlayer = 'O'
-            playerTurn.innerHTML = 'O'
+        if (currentPlayer === 'X'){   // condição que verifica se o currentPlayer é igual a 'X'
+            currentPlayer = 'O'       // caso seja satisfeita é reatribuído o valor 'O' para a variavel currentPlayer
+            playerTurn.innerHTML = 'O' // atualiza no HTML que a vez é de 'O'
             
-        }else {
-            currentPlayer = 'X'
-            playerTurn.innerHTML = 'X'
+        }else {                         // Se a primeira condição não for satisfeita então essa é satisfeita
+            currentPlayer = 'X'         // o valor da variável currentPlayer é reatribuído com o valor 'X'
+            playerTurn.innerHTML = 'X'  // atualiza no HTML que a vez é de 'X'
+            
         }
-
-
-        element.removeEventListener('click' , changePlayer)
-
-        checkWinner();
         
+        checkWinner();   // chamada da função checkWinner() que verifica se houve vencedor ou empate. Caso haja vencedor retorna o vencedor e se houve empate retorna que empatou.
+        
+       
     }
 
-    function checkWinner () {
-        let roundWon = false ;
-       function winningMessage (){
-           return ` ${board[i]} venceu! `;
-        }
+    function checkWinner () {  // função que verifica se houve vencedor ou empate.
+        
+        let roundWon = false ; // variavel
 
         for (let i = 0; i <= 7; i++){
             let winCondition = winningConditions[i];
@@ -64,58 +57,56 @@ for (let i = 0; i < boxes.length; i++) {
             }
             if (a === b && b === c) {
                 roundWon = true;
-                break;
+                playing = false
+                break
             }
-           
+            
         }
 
-        if (roundWon) {
-            winnerAnounce.innerHTML = winningMessage ();
-            gameOn = false ;
-            return false;
+        function winner () {
+            let roundDraw = !board.includes("");
+
+            if (roundWon) {                 //condição que retorna uma mensagem com o vencedor da partida.
+                winnerAnnouce.innerHTML = `${board[i]} venceu !`;
+                playerTurn.innerHTML = '' ;
+                gameOn = false;
+                return ;
+                
+            }else if (roundDraw) {      //condição que verifica se houve empate e retorna uma mensagem.
+                winnerAnnouce.innerHTML = 'Empatou' ;
+                playerTurn.innerHTML = ''
+                gameOn = false ;
+                return;
+                
+            }
 
         }
-
-        if (winningMessage === 'X'){
-            element.removeEventListener('click' , changePlayer ())
-        }
-
-        let roundDraw = !board.includes("");
-
-        if(roundDraw) {
-            winnerAnounce.innerHTML = 'Empatou';
-           
-            gameOn = false; 
-            return;
-        }
-
-        
-        
        
+        winner(); // chamada da funcão winner que retorna se houve vencedor ou empate.
+
+       
+    
+
     }
 
-    function handleRestartGame() {
-        gameOn = true;
+    
+      
+    
+    function restartGame() {   // função que reinicia o jogo transformando-o para o seu estado inicial.
+        
         currentPlayer = "X";
         board = ["", "", "", "", "", "", "", "", ""];
         element.innerHTML = '';
-        playerTurn.innerHTML = ''
-        winnerAnounce.innerHTML = ''
-
-        document.querySelectorAll('box').forEach(box => box.innerHTML = "");
-
-        
-        element.addEventListener('click', changePlayer)
-
+        playerTurn.innerHTML = 'X'
+        winnerAnnouce.innerHTML = ''
+        element.addEventListener('click', changePlayer , {once: true})
 
     }
 
-
-    resetBtn.addEventListener('click' , handleRestartGame)
-   
-    
-  element.addEventListener('click', changePlayer)
-  
-
+  resetBtn.addEventListener('click' , restartGame);
+  element.addEventListener('click', changePlayer , {once: true})  // 
   
 }
+
+
+
